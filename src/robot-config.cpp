4894f68af818c7c393f286961260a1c6c1c00de1
eddie280 <nguyeneddie04@gmail.com>
2022-01-24 16:59:10 -0800

    more changes

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -13,13 +13,16 @@ motor FR = motor(PORT1, ratio18_1, true);
 motor FL = motor(PORT2, ratio18_1, false);
 motor BR = motor(PORT3, ratio18_1, true);
 motor BL = motor(PORT4, ratio18_1, false);
-motor BAL = motor(PORT6, ratio18_1, false);
-motor FAR = motor(PORT7, ratio18_1, false);
-motor FAL = motor(PORT8, ratio18_1, true);
+motor BAL = motor(PORT6, ratio36_1, true);
+motor FAR = motor(PORT7, ratio36_1, true);
+motor FAL = motor(PORT8, ratio36_1, false);
 /*vex-vision-config:begin*/
-vision vision_1 = vision (PORT11, 50);
+signature vision_1__MOBILE_GOAL = signature (1, 255, 1057, 656, -2595, -1371, -1983, 2.5, 0);
+signature vision_1__RED_GOAL = signature (2, 4647, 7711, 6179, 445, 1431, 938, 2.5, 0);
+signature vision_1__BLUE_GOAL = signature (3, -2941, -1869, -2405, 6225, 12529, 9377, 1.4, 0);
+vision vision_1 = vision (PORT11, 50, vision_1__MOBILE_GOAL, vision_1__RED_GOAL, vision_1__BLUE_GOAL);
 /*vex-vision-config:end*/
-motor FC = motor(PORT5, ratio36_1, false);
+motor FC = motor(PORT5, ratio36_1, true);
 
 // VEXcode generated functions
 // define variable for remote controller enable/disable
